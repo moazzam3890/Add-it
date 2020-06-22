f03a501a0fb0ffdcc026ad53b97abc6d0d09de5f
Moazzam Adil Khan <moaadi3890@gmail.com>
2020-06-23 01:11:43 +0500

    Request handler with get request, function and route has been defined

--- add-it/src/main.rs
+++ add-it/src/main.rs
@@ -2,6 +2,13 @@
 
 #[macro_use] extern crate rocket;
 
+#[get("/submit/<num>")]
+fn number(num: isize) -> String {
+    let result = num +50;
+
+    format!("Your result is {}", result)
+}
+
 fn main() {
     rocket::ignite()
     .mount("/", routes![number])
